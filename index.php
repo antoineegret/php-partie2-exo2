5de904ba249aea510a4cced9d8df073f7d0ac1f6
antoineegret <a.egret@laposte.net>
2019-12-09 12:04:40 +0100

    exo2 partie2 php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $isEasy == false;
+    if ($isEasy == true) {
+        echo "sa marche tabernacle";
+    }
+    if ($isEasy == false) {
+        echo "calice Ça va pas bin mon affaire";
+    }
+    ?>
+</body>
+</html>
